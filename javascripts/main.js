4a7a7352c274201c7ac509bf2bee32e9f65f234c
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2016-03-08 17:54:28 +0000

    simple profile page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+function loadGoogleAnalytics() {
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+}
+
+function loadPageTracker() {
+    try {
+      var pageTracker = _gat._getTracker("UA-70419676-1");
+      pageTracker._trackPageview();
+    } catch(err) {}
+}
